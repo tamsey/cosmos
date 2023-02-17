1811907931dd228692375d2c3d078dcccca435c4
tamsey <tamannasharma1492@gmail.com>
2023-02-17 07:05:22 +0000

    updated

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,13 +1,34 @@
 provider "azurerm" {
     features {}
 }
-resource "azurerm_resource_group" "newresource" {
-  location = "eastus"
-  name = "tamanna"
+variable "location" {
+  default = "eastus"
+}
+variable "name" {
+  default = "new_resource"
+}
+resource "azurerm_resource_group" "thinknyx" {
+  location = var.location
+  name = var.name
   tags = {
-    Name = "Day"
+    Name = "Training"
     Day = "5"
   }
 }
-
-
+resource "azurerm_cosmosdb_account" "GlobalDocumentDB" {
+  name                      = var.name
+  location                  = var.location
+  resource_group_name       = azurerm_resource_group.thinknyx.name
+  offer_type                = "Standard"
+  kind                      = "GlobalDocumentDB"
+  enable_automatic_failover = false
+  geo_location {
+    location          = var.location
+    failover_priority = 0
+  }
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+}
